2e853ba259048fe4430fd9c5b1db54db4fbf2059
Alan Felix <felixnascc@gmail.com>
2022-07-02 17:25:13 -0300

    Recurso de adicionar tarefas na lista

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,17 +1,40 @@
 import React from 'react';
+import { ITarefa } from '../../types/tarefa';
 import Botao from '../Botao';
 import style from './Formulario.module.scss';
 
-class Formulario extends React.Component {
+class Formulario extends React.Component<{
+  setTarefas: React.Dispatch<React.SetStateAction<ITarefa[]>>;
+}> {
+  state = {
+    tarefa: '',
+    tempo: '00:00:00',
+  };
+
+  adicionarTarefa(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    this.props.setTarefas((tarefasAntigas) => [
+      ...tarefasAntigas,
+      { ...this.state },
+    ]);
+  }
+
   render() {
     return (
-      <form className={style.novaTarefa}>
+      <form
+        className={style.novaTarefa}
+        onSubmit={this.adicionarTarefa.bind(this)}
+      >
         <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
           <input
             type="text"
             name="tarefa"
             id="tarefa"
+            value={this.state.tarefa}
+            onChange={(e) =>
+              this.setState({ ...this.state, tarefa: e.target.value })
+            }
             placeholder="O que você quer estudar"
             required
           />
@@ -23,12 +46,16 @@ class Formulario extends React.Component {
             step="1"
             name="tempo"
             id="tempo"
+            value={this.state.tempo}
+            onChange={(e) =>
+              this.setState({ ...this.state, tempo: e.target.value })
+            }
             min="00:00:00"
             max="01:30:00"
             required
           />
         </div>
-        <Botao>Adicionar</Botao>
+        <Botao type="submit">Adicionar</Botao>
       </form>
     );
   }
