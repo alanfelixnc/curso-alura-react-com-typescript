59e1a097f1a855593bc66aa8e31138f3366b2a5a
Alan Felix <felixnascc@gmail.com>
2022-07-02 12:03:23 -0300

    Criação do componente Formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Formulario />
+    </div>
+  );
 }
 
 export default App;
